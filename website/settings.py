640098e5858a58e860a53f4fb68f2d854cc5eafb
Kamila Sulaimanova <kamilasulaimanova@Kamilas-MacBook-Pro.local>
2022-06-12 04:34:37 +0600

    Heroku2

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j=l%&fcc_v=6=7i3ft%@v12vc*pot$fj*l&^2sr(!_9!%*&fsz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['meeting-club.herokuapp.com', '127.0.0.1']
 
